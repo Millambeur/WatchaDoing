3dbb679ef76e326ef06b3b1ee80d7548d5dec7e1
Millambeur <florian.chassot@gmail.com>
2016-02-22 19:36:26 +0100

    Adding UserBundle v 0.0.1

---
+++ src/Altgeek/UserBundle/AltgeekUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Altgeek\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AltgeekUserBundle extends Bundle
+{
+}
---
+++ src/Altgeek/UserBundle/Controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Altgeek\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AltgeekUserBundle:UserViews:index.html.twig');
+    }
+}
