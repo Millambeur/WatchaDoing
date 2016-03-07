9a91a8c5b86ceee18f76cfd19c79835eb2531d05
Millambeur <florian.chassot@gmail.com>
2016-03-07 15:38:33 +0100

    all templates extends index.html.twig

--- src/Altgeek/CoreBundle/Controller/CoreController.php
+++ src/Altgeek/CoreBundle/Controller/CoreController.php
@@ -8,6 +8,6 @@ class CoreController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AltgeekCoreBundle:CoreViews:index.html.twig');
+        return $this->render('AltgeekCoreBundle::index.html.twig');
     }
 }
